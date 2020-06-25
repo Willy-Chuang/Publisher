bd45086d74d19d70715aec7b9a1a3c13e6dcc068
Willy-Chuang <ws18950914@gmail.com>
2020-06-25 10:09:26 +0800

    Successfully connected to firestore

--- Publisher/app/src/main/java/com/willy/publisher/MainActivity.kt
+++ Publisher/app/src/main/java/com/willy/publisher/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.willy.publisher
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.willy.publisher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.textHelloWorld.setOnClickListener {
+            Log.i("button","clicked")
+            viewModel.addData()
+        }
+
     }
 }
